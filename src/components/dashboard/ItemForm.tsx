c3f2aeef3394d5dc5c0e503b2cb1ad24c5a5444d
Ahmd Abdo <rehlathadaf@gmail.com>
2026-01-07 14:35:57 +0200

    Fix button text for adding/updating item

--- src/components/dashboard/ItemForm.tsx
+++ src/components/dashboard/ItemForm.tsx
@@ -547,7 +547,7 @@ export function ItemForm({ catalogId, catalogPlan, categories, item, onSuccess, 
                   <span>جاري الحفظ...</span>
                 </div>
               ) : (
-                item ? 'تحديث المنتج' : 'إضافة المنتج للمنيو'
+                item ? 'تحديث المنتج' : 'إضافة المنتج '
               )}
             </Button>
           </div>
